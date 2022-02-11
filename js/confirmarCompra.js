a81d7f53bf7a8c13264a2a07f75f81ab7d7167c0
Cristian Dalmaso <c.dalmaso3@gmail.com>
2022-02-11 17:37:53 -0300

    Añade total a Local Storage y modifica página de finalización de compra

--- js/confirmarCompra.js
+++ js/confirmarCompra.js
@@ -1,47 +1,50 @@
 //Seleccion medio de pago
-const medioPago=document.querySelector("#medio-pago")
+const medioPago = document.querySelector("#medio-pago");
 medioPago.addEventListener("change", (e) => {
-    if(e.target.value==="1"){
-        let datosPago = document.getElementById("contenedor-datos-pago");
-        datosPago.innerHTML=` 
+  if (e.target.value === "1") {
+    let datosPago = document.getElementById("contenedor-datos-pago");
+    datosPago.innerHTML = ` 
         <label for="datosTarjetaCredito" class="col-sm-2 col-form-label"></label>
-        <div class="col-sm-10" style="margin-bottom: 1rem">
-        <span>Ingrese numero de tarjeta de crédito</span>
+        <div class="datos-tarjeta" style="margin-bottom: 1rem">
+        <span">Ingrese numero de tarjeta de crédito</span>
           <input type="text" class="form-control" id="inputPassword">
         </div>
         <label for="CVC" class="col-sm-2 col-form-label"></label>
-        <div class="col-sm-10">
+        <div class="datos-tarjeta">
         <span>Ingrese CVC</span>
           <input type="password" class="form-control" id="inputPassword">
         </div>
         `;
-    }
-    else if(e.target.value==="2"){
-        let datosPago = document.getElementById("contenedor-datos-pago");
-        datosPago.innerHTML=` 
+  } else if (e.target.value === "2") {
+    let datosPago = document.getElementById("contenedor-datos-pago");
+    datosPago.innerHTML = ` 
         <label for="datosBilleteraBitcoin" class="col-sm-2 col-form-label"></label>
-        <div class="col-sm-10">
+        <div class="datos-tarjeta">
         <span>Ingrese numero de billetera BTC</span>
           <input type="text" class="form-control" id="inputPassword">
         </div>
         `;
-    }
-    else if(e.target.value==="3"){
-        let datosPago = document.getElementById("contenedor-datos-pago");
-        datosPago.innerHTML=` 
+  } else if (e.target.value === "3") {
+    let datosPago = document.getElementById("contenedor-datos-pago");
+    datosPago.innerHTML = ` 
         <label for="datosBilleteraEthereum" class="col-sm-2 col-form-label"></label>
-        <div class="col-sm-10">
+        <div class="datos-tarjeta">
         <span>Ingrese numero de billetera ETH</span>
           <input type="text" class="form-control" id="inputPassword">
         </div>
         `;
-    }
-})
+  }
+});
+
+const totalaPagar = document.getElementById("totalPagar");
+totalaPagar.innerHTML = `Total a pagar: ${localStorage.getItem(
+  "totalCompra"
+)} USD`;
 
 let realizaPago = document.getElementById("realizar-pago");
-realizaPago.addEventListener("click", ()=>{
+realizaPago.addEventListener("click", () => {
   Swal.fire({
-    icon: 'success',
-    html: `<p class="texto-alert"> Pago confirmado. Muchas gracias por su compra</p>`
-})
-} )
+    icon: "success",
+    html: `<p class="texto-alert"> Pago confirmado. Muchas gracias por su compra</p>`,
+  });
+});
